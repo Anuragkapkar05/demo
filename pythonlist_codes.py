08f042fa931e1f581a55fe7bc7700aa4a51b0c67
Anuragkapkar05 <anuragkapkar0507@gmail.com>
2025-10-30 11:54:06 +0530

    first commit

---
+++ pythonlist_codes.py
@@ -0,0 +1,25 @@
+a1=[1,2,3,4,5]
+a1.append(6)
+print(a1)
+
+a1.extend ([7,8,9])
+print(a1)
+
+a1.pop()
+print(a1)
+a1.pop(2) #pop is remove by index
+print(a1)
+a1[1]
+print(a1)
+a1[3]
+
+b1=[10,'anurag',20.5,True,[1,2,3],(4,5,6)]
+b1[1]
+print(b1)
+b1[4][1]
+
+second_item=b1[2]
+print(second_item)
+a= 'hello'
+print(a[1])
+print(b1[-1])
